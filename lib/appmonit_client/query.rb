793b2d5ec08d2ec7b7d049659f6e1c4e4eeff4d4
Benoist Claassen <benoist.claassen@gmail.com>
2014-03-07 14:58:39 +0100

    refactor

--- lib/appmonit_client/query.rb
+++ lib/appmonit_client/query.rb
@@ -3,12 +3,16 @@ module AppmonitClient
     class << self
       %w(count count_unique minimum maximum average sum funnel).each do |method_name|
         define_method method_name do |collection_name, params|
-          path = "/v1/queries/#{method_name}"
-
-          params[:event_collection] = collection_name
-
-          Http.get(path, params)
+          query(method_name, collection_name, params)
         end
+      end
+
+      def query(method_name, collection_name, params)
+        path = "/v1/queries/#{method_name}"
+
+        params[:event_collection] = collection_name
+
+        Http.get(path, params)
       end
     end
   end
