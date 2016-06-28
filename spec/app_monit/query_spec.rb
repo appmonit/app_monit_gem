2ec36d8898bb211afb86ae77acf9a2bfe51ec3b8
Tom Mestdagh <tmestdagh@digidentity.com>
2016-06-28 16:13:57 +0200

    Only add the `Appmonit-Api-Key` header when there is no `api_key` added to the params.

--- spec/app_monit/query_spec.rb
+++ spec/app_monit/query_spec.rb
@@ -9,17 +9,36 @@ describe AppMonit::Query do
     AppMonit::Config.env       = nil
   end
 
-  %w(count count_unique minimum maximum average sum funnel).each do |method_name|
-    describe method_name do
-      it 'gets the results with the given params' do
-        stub_request(:get, /api.appmon.it\/v1\/queries\/#{method_name}/).to_return(body: {result: '0'}.to_json)
+  describe 'No api_key has been added to the params' do
+    %w(count count_unique minimum maximum average sum funnel).each do |method_name|
+      describe method_name do
+        it 'gets the results with the given params' do
+          stub_request(:get, /api.appmon.it\/v1\/queries\/#{method_name}/).to_return(body: {result: '0'}.to_json)
 
-        params = { valid: 'params' }
-        subject.send(method_name, 'collection_name', params)
+          params = { valid: 'params' }
+          subject.send(method_name, 'collection_name', params)
 
-        params[:event_collection] = 'collection_name'
+          params[:event_collection] = 'collection_name'
 
-        assert_requested(:get, /api.appmon.it\/v1\/queries\/#{method_name}\?query=#{params.to_json}/)
+          assert_requested(:get, /api.appmon.it\/v1\/queries\/#{method_name}\?query=#{params.to_json}/)
+        end
+      end
+    end
+  end
+
+  describe 'An api_key has been added to the params' do
+    %w(count count_unique minimum maximum average sum funnel).each do |method_name|
+      describe method_name do
+        it 'gets the results with the given params' do
+          stub_request(:get, /api.appmon.it\/v1\/queries\/#{method_name}/).to_return(body: {result: '0'}.to_json)
+
+          params = { valid: 'params', api_key: 'XXX' }
+          subject.send(method_name, 'collection_name', params)
+
+          params[:event_collection] = 'collection_name'
+
+          assert_requested(:get, /api.appmon.it\/v1\/queries\/#{method_name}\?api_key=XXX&query=#{params.to_json}/)
+        end
       end
     end
   end
